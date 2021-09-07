c0003a500fd3e7df0c88ab0c547d41cb8c73a642
Neo <petrenko-m-p@mail.ru>
2021-09-07 17:16:19 +0700

    Initial commit

---
+++ charts_test/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+
+namespace charts_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void assignRandomValues()
+        {
+            var values = new double[100];
+            var r = new Random();
+            var t = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                t += r.Next(-10, 10);
+                values[i] = t;
+            }
+
+            var SeriesCollection = new ISeries[]
+            {
+                new LineSeries<double> {Values = values, Fill = null, LineSmoothness = 0},
+            };
+
+            chart_plot.plot.Series = SeriesCollection;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            assignRandomValues();
+        }
+    }
+}
---
+++ charts_test/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+
+namespace charts_test
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            var values = new double[100];
+            var r = new Random();
+            var t = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                t += r.Next(-10, 10);
+                values[i] = t;
+            }
+
+            SeriesCollection = new ISeries[]
+            {
+                new LineSeries<double> {Values = values, Fill = null, LineSmoothness = 0},
+                new ScatterSeries<ObservablePoint>
+                {
+                    Values = new ObservableCollection<ObservablePoint>
+                    {
+                        new ObservablePoint(2.2, 5.4),
+                        new ObservablePoint(4.5, 2.5),
+                        new ObservablePoint(4.2, 7.4),
+                        new ObservablePoint(6.4, 9.9),
+                        new ObservablePoint(4.2, 9.2),
+                        new ObservablePoint(5.8, 3.5),
+                        new ObservablePoint(7.3, 5.8),
+                        new ObservablePoint(8.9, 3.9),
+                        new ObservablePoint(6.1, 4.6),
+                        new ObservablePoint(9.4, 7.7),
+                    }
+                }
+            };
+            
+        }
+
+        public IEnumerable<ISeries> SeriesCollection { get; set; }
+    }
+}
