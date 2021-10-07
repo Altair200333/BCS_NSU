c90be720b7d437a484d7df0340b4e21c24f7b0a3
Neo <petrenko-m-p@mail.ru>
2021-10-08 01:24:36 +0700

    solver

--- charts_test/Windows/Task_9.xaml.cs
+++ charts_test/Windows/Task_9.xaml.cs
@@ -15,51 +15,55 @@ using Color = System.Drawing.Color;
 
 namespace charts_test.Windows
 {
-    class KurwaSolver
+    class SecondOrderEquation
     {
+        //y'' + p(x) * y' + q(x) * y = r(x)
         public Func<double, double> px = x => 0.0;
         public Func<double, double> qx = x => 0.0;
-        public Func<double, double> rx = x => Math.Sin(x);
+        public Func<double, double> rx = x => 0;
 
-        public double a = 0;
-        public double b = Math.PI;
 
-        public double ct1 = 1, ct2 = 0, ct = 0;
-        public double dt1 = 1, dt2 = 0, dt = 0;
+        //ct1 * y(a) + ct2 * y'(a) = ct
+        //dt1 * y(b) + dt2 * y'(b) = dt
+        public double a;
+        public double b;
+
+        public double ct1, ct2, ct;
+        public double dt1, dt2, dt;
+    }
+    class RunnerSolver
+    {
         public int n = 5;
 
         private double h;
 
-        double ai(double xi)
+        double ai(SecondOrderEquation equation, double xi)
         {
-            return 1.0 / (h * h) - px(xi) / (2.0 * h);
+            return 1.0 / (h * h) - equation.px(xi) / (2.0 * h);
         }
 
-        double bi(double xi)
+        double bi(SecondOrderEquation equation, double xi)
         {
-            return 2.0 / (h * h) - qx(xi);
+            return 2.0 / (h * h) - equation.qx(xi);
         }
 
-        double ci(double xi)
+        double ci(SecondOrderEquation equation, double xi)
         {
-            return 1.0 / (h * h) + px(xi) / (2.0 * h);
+            return 1.0 / (h * h) + equation.px(xi) / (2.0 * h);
         }
 
-        double ri(double xi)
+        
+        double di(SecondOrderEquation equation, double xi)
         {
-            return rx(xi);
-        }
-        double di(double xi)
-        {
-            return rx(xi);
+            return equation.rx(xi);
         }
 
-        public List<Vector> solve()
+        public List<Vector> solve(SecondOrderEquation equation)
         {
-            h = (b - a) / n;
+            h = (equation.b - equation.a) / n;
 
-            double e1 = -ct2 / (ct1 * h - ct2);
-            double n1 = ct * h / (ct1 * h - ct2);
+            double e1 = -equation.ct2 / (equation.ct1 * h - equation.ct2);
+            double n1 = equation.ct * h / (equation.ct1 * h - equation.ct2);
 
             List<double> es = new List<double>();
             List<double> ns = new List<double>();
@@ -69,28 +73,28 @@ namespace charts_test.Windows
 
             for (int i = 1; i <= n - 1; i++)
             {
-                double xi = a + (i) * h;
+                double xi = equation.a + (i) * h;
                 double lastE = es.Last();
                 double lastN = ns.Last();
 
-                double e = ci(xi) / (bi(xi) - ai(xi) * lastE);
-                double n = (lastN * ai(xi) - di(xi)) / (bi(xi) - ai(xi) * lastE);
+                double e = ci(equation, xi) / (bi(equation, xi) - ai(equation, xi) * lastE);
+                double n = (lastN * ai(equation, xi) - di(equation, xi)) / (bi(equation, xi) - ai(equation, xi) * lastE);
 
                 es.Add(e);
                 ns.Add(n);
             }
 
-            double yn = (dt2 * ns.Last() - dt * h) / (dt2 * (1 - es.Last()) + dt1 * h);
+            double yn = (equation.dt2 * ns.Last() + equation.dt * h) / (equation.dt2 * (1 - es.Last()) + equation.dt1 * h);
             double[] result = new double[n + 1];
             List<Vector> output = new List<Vector>();
 
             result[n] = yn;
-            output.Add(new Vector(a + (n) * h, yn));
+            output.Add(new Vector(equation.a + (n) * h, yn));
 
             for (int i = n; i >= 1; --i)
             {
                 result[i - 1] = result[i] * es[i - 1] + ns[i - 1];
-                double xi = a + (i - 1) * h;
+                double xi = equation.a + (i - 1) * h;
 
                 output.Add(new Vector(xi, result[i - 1]));
             }
@@ -103,21 +107,30 @@ namespace charts_test.Windows
 
     public partial class Task_9 : Window
     {
-        KurwaSolver s = new KurwaSolver();
+        RunnerSolver s = new RunnerSolver();
 
+        private SecondOrderEquation equation = new SecondOrderEquation()
+        {
+            rx = x => Math.Sin(x),
+            a = 0,
+            b = Math.PI,
+
+            ct1 = 1,
+            dt1 = 1,
+        };
         public Task_9()
         {
             InitializeComponent();
 
             y_a_value.ValueChanged += delegate(object sender, RoutedPropertyChangedEventArgs<double> args)
             {
-                s.ct = y_a_value.Value;
+                equation.ct = y_a_value.Value;
                 resolveAndPlot();
             };
 
             y_b_value.ValueChanged += delegate (object sender, RoutedPropertyChangedEventArgs<double> args)
             {
-                s.dt = y_b_value.Value;
+                equation.dt = y_b_value.Value;
                 resolveAndPlot();
             };
             n_value.ValueChanged += delegate(object sender, RoutedPropertyChangedEventArgs<double> args)
@@ -131,7 +144,7 @@ namespace charts_test.Windows
 
         private void resolveAndPlot()
         {
-            var res = s.solve();
+            var res = s.solve(equation);
             PlotTools.clear(WpfPlot1);
             PlotTools.plotFunction(res, Color.Red, 2, WpfPlot1);
         }
